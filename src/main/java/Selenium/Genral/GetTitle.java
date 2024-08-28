58c28d08b39c8af2c4d8d05db101abc915f9334a
prati <pratiksonone48@gmail.com>
2024-08-28 16:46:51 +0530

    Third commit for my selenium code

---
+++ src/main/java/Selenium/Genral/GetTitle.java
@@ -0,0 +1,35 @@
+package Selenium.Genral;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class GetTitle
+
+{
+	public static void main(String[] args) 
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.flipkart.com/");
+		
+		
+		//Implicit Wait
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		WebElement hh=driver.findElement(By.xpath("dsfsf"));
+		//Explicit Wait
+		WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(10));
+		
+		System.out.println(driver.getTitle());
+		driver.quit();
+		
+		
+	}
+
+}
